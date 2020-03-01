d4dbaed75b381192f900517a55e72ea3d2af5727
kashaf12 <kashafaahmed@gmail.com>
2020-03-01 18:44:18 +0530

    first blockchain smoke test done

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    //store candidate
+    //read candidate
+    //constructor
+    string public candidate;
+
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+
+}
