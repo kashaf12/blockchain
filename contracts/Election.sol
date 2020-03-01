5ff3a9c2405e25870efc2e60291fc09476c1d41f
kashaf12 <kashafaahmed@gmail.com>
2020-03-01 23:15:28 +0530

    added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,47 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Election {
-    //store candidate
-    //read candidate
-    //constructor
-    string public candidate;
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+    mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint256 public candidatesCount;
+
+    // voted event
+    event votedEvent(uint256 indexed _candidateId);
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
     }
 
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender], "already voted");
+
+        // require a valid candidate
+        require(
+            _candidateId > 0 && _candidateId <= candidatesCount,
+            "candidate not available"
+        );
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
 }
